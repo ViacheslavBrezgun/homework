3be191bee9c5e496858a0cb38a6b98c6c343b5c1
Viacheslav Brezgun <korben2222@gmail.com>
2023-04-25 21:42:18 +0200

    Изменение в коде 4й задачи

--- src/Home_Work_1/FourTask/FourTask4.java
+++ src/Home_Work_1/FourTask/FourTask4.java
@@ -4,8 +4,8 @@ package Home_Work_1.FourTask;
 public class FourTask4 {
     public static void main(String[] args) {
         int myNumber = 100240;
-        int reskByte = myNumber / 1024;
-        int resByte = myNumber * 1024;
-        System.out.println("Байт : " + resByte + " килобайт: " + reskByte);
+        int kByte = myNumber / 1024;
+        int Byte = myNumber * 1024;
+        System.out.println("Байт: " + Byte + " килобайт: " + kByte);
     }
 }
