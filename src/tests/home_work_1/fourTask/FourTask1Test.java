4f7bd12cf9c64d56b28ff28526e98398dce5d9cf
Viacheslav Brezgun <korben2222@gmail.com>
2023-05-24 16:24:38 +0200

    ДЗ-3 тесты

---
+++ src/tests/home_work_1/fourTask/FourTask1Test.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FourTask1Test {
+    
+    int[] actual = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int[] expect = { 1, 3, 5, 7, 9};
+    @Test
+    void main() {
+        for (int arr:expect) {
+            Assertions.assertTrue(arr%2!=0);
+        }
+    }
+}
