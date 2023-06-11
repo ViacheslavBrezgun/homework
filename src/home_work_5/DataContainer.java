8cf627d96328938071b26fed15b4b1e7f778f301
Viacheslav Brezgun <korben2222@gmail.com>
2023-06-11 23:30:12 +0200

    ...

--- src/home_work_5/DataContainer.java
+++ src/home_work_5/DataContainer.java
@@ -2,23 +2,24 @@ package home_work_5;
 
 import java.util.Arrays;
 
+import static java.util.Arrays.copyOf;
+
 public class DataContainer<T> {
     private T[] data;
-    private T x;
-    private int k = 0;
-    private int i = 0;
 
     public DataContainer(T[] data) {
         this.data = data;
     }
 
     public T get(int index) {
+        T x;
         if (index <= data.length - 1) {
             x = data[index];
         } else {
             x = null;
         }
         return x;
+
     }
 
     public T[] getItems() {
@@ -26,44 +27,36 @@ public class DataContainer<T> {
     }
 
     public boolean delete(int index) {
-        for (i = index; i < data.length - 1; i++) {
+        for (int i = index; i < data.length - 1; i++) {
             data[i] = data[i + 1];
         }
         data[data.length - 1] = null;
         return true;
     }
 
-    public int add(T item) {
-        //int flag=0;
-        if (item != " ") {
-            for (int i = 0; i < data.length; i++) {
-                if (data[i] == null) {
-                    data[i] = item;
-                    k = i;
-                    break;
-                }
+
+    public int add(T item){
+        if(item==null){
+            return -1;
+        }
+
+        int index=-1;
+
+        for (int i = 0; i < this.data.length; i++) {
+            if(this.data[i]==null){
+                index=i;
+                break;
 
             }
-
-        } else {
-            k = -1;
-            System.out.println("Нельзя");
         }
-/**
- * РАСШИРЕНИЕ МАСИВА
- */
-
-        T[] data2 = Arrays.copyOf(data, data.length + 1);
-        data2[data2.length - 1] = item;
-        k = data2.length - 1;
-        for (int i = 0; i < data2.length; i++) {
-            System.out.println(data2[i]);
-
+        if(index==-1){
+            index=this.data.length;
+            this.data=copyOf(this.data, this.data.length+1);
         }
-        return k;
+        this.data[index]=item;
+        return index;
     }
 }
-
 
 
 
